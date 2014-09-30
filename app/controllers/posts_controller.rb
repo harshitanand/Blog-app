49fc08de3b237014275efce5e64d715d23553dca
harshit03 <harshitanand94@gmail.com>
2014-09-30 09:32:18 +0530

    Added authentication

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate, accept: [:index,:show]
   # GET /posts
   # GET /posts.json
   def index
@@ -70,5 +70,10 @@ class PostsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
       params.require(:post).permit(:title, :body)
+    end
+    def authenticate
+      authenticate_or_request_with_http_basic do |name,password|
+        name == "admin" && password == "secret"
+      end
     end
 end
